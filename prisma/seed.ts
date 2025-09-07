63e91124295efb0b81541918dd7eeee3654cdf14
David Zeff <dpzeff@gmail.com>
2025-09-07 23:11:02 +0300

    .

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
-import PrismaClient from "@prisma/client";
-const prisma = new PrismaClient.PrismaClient();
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 async function main() {
   console.log("🌱 Seeding database...");
